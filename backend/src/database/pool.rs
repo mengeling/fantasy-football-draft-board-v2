aedeb26a69a83078df00d968d0874420935c9386
mengeling <mengeling@thezebra.com>
2024-10-29 17:06:44 -0500

    Update create table queries

--- backend/src/database/pool.rs
+++ backend/src/database/pool.rs
@@ -29,11 +29,61 @@ pub async fn init_db() -> Result<(), sqlx::Error> {
     .await?;
 
     sqlx::query(
-        "CREATE TABLE IF NOT EXISTS player_stats (
-            player_id INTEGER REFERENCES players(id),
-            stat_name TEXT NOT NULL,
-            stat_value DOUBLE PRECISION,
-            PRIMARY KEY (player_id, stat_name)
+        "CREATE TABLE IF NOT EXISTS rankings (
+            player_id INTEGER PRIMARY KEY REFERENCES players(id),
+            overall INTEGER,
+            position INTEGER
+        )",
+    )
+    .execute(&*DB_POOL)
+    .await?;
+
+    sqlx::query(
+        "CREATE TABLE IF NOT EXISTS stats (
+            player_id INTEGER PRIMARY KEY REFERENCES players(id),
+            pass_cmp DOUBLE PRECISION,
+            pass_att DOUBLE PRECISION,
+            pass_cmp_pct DOUBLE PRECISION,
+            pass_yds DOUBLE PRECISION,
+            pass_yds_per_att DOUBLE PRECISION,
+            pass_td DOUBLE PRECISION,
+            pass_int DOUBLE PRECISION,
+            pass_sacks DOUBLE PRECISION,
+            rush_att DOUBLE PRECISION,
+            rush_yds DOUBLE PRECISION,
+            rush_yds_per_att DOUBLE PRECISION,
+            rush_long DOUBLE PRECISION,
+            rush_20 DOUBLE PRECISION,
+            rush_td DOUBLE PRECISION,
+            fumbles DOUBLE PRECISION,
+            receptions DOUBLE PRECISION,
+            rec_tgt DOUBLE PRECISION,
+            rec_yds DOUBLE PRECISION,
+            rec_yds_per_rec DOUBLE PRECISION,
+            rec_long DOUBLE PRECISION,
+            rec_20 DOUBLE PRECISION,
+            rec_td DOUBLE PRECISION,
+            field_goals DOUBLE PRECISION,
+            fg_att DOUBLE PRECISION,
+            fg_pct DOUBLE PRECISION,
+            fg_long DOUBLE PRECISION,
+            fg_1_19 DOUBLE PRECISION,
+            fg_20_29 DOUBLE PRECISION,
+            fg_30_39 DOUBLE PRECISION,
+            fg_40_49 DOUBLE PRECISION,
+            fg_50 DOUBLE PRECISION,
+            extra_points DOUBLE PRECISION,
+            xp_att DOUBLE PRECISION,
+            sacks DOUBLE PRECISION,
+            int DOUBLE PRECISION,
+            fumbles_recovered DOUBLE PRECISION,
+            fumbles_forced DOUBLE PRECISION,
+            def_td DOUBLE PRECISION,
+            safeties DOUBLE PRECISION,
+            special_teams_td DOUBLE PRECISION,
+            games DOUBLE PRECISION,
+            fantasy_pts DOUBLE PRECISION,
+            fantasy_pts_per_game DOUBLE PRECISION
         )",
     )
     .execute(&*DB_POOL)
